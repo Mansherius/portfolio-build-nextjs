abf2b98f5919b044a5b91da40dcbec69676f88c8
Mansher Singh <Manshers-MacBook-Pro.local>
2024-07-01 15:41:02 +0530

    Functioning popup navbar. not very smooth

---
+++ portfolio-build/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Mansher | Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
